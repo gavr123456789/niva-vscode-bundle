e830f79e6adb59094b1727f0698d5da4ee64db57
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-12-04 17:20:58 +0200

    NivaHoverProvider

--- extension/extension.js
+++ extension/extension.js
@@ -17,4 +17,28 @@ exports.activate = function (context) {
     context.subscriptions.push(diagnostics)
 
     console.log("[Niva-Lint] Initialized.")
+
+    /** @implements {HoverProvider} */
+    class NivaHoverProvider {
+        /**
+        * @param {vscode.TextDocument} document
+        */
+        provideHover(document, position, token) {
+            const __range = document.getWordRangeAtPosition(position)
+            const __word = document.getText(__range)
+
+            return new Promise((resolve, reject) => {
+                        reject()
+            })
+        }
+    }
+
+    const providerHover = vscode.languages.registerHoverProvider(
+        sel,
+        new NivaHoverProvider()
+    )
+
+    context.subscriptions.push(
+        providerHover,
+    )
 }
