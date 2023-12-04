6624b7bfeea3e93ee2500b2ff6e601e822385aa9
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-12-04 17:19:42 +0200

    Update extension.js

--- extension/extension.js
+++ extension/extension.js
@@ -7,3 +7,14 @@ const {
     Hover
 } = require('vscode')
 
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const url = require('url')
+
+exports.activate = function (context) {
+    const diagnostics = vscode.languages.createDiagnosticCollection('niva')
+    context.subscriptions.push(diagnostics)
+
+    console.log("[Niva-Lint] Initialized.")
+}
