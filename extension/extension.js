eb62d0d30f66ca0f6c65c5bb283692d87fa673f7
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2023-12-04 17:24:39 +0200

    CompletionItemProvider example

--- extension/extension.js
+++ extension/extension.js
@@ -19,6 +19,37 @@ exports.activate = function (context) {
     console.log("[Niva-Lint] Initialized.")
 
 
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('vshexa.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World from Niva!')
+    })
+    // context.subscriptions.push(disposable);
+
+    const provider2 = vscode.languages.registerCompletionItemProvider(
+        sel,
+        {
+            provideCompletionItems(document, position) {
+                // get all text until the `position` and check if it reads `console.`
+                // and if so then complete if `log`, `warn`, and `error`
+                const linePrefix = document.lineAt(position).text.substr(0, position.character)
+                if (!linePrefix.endsWith('gavr.')) {
+                    return undefined
+                }
+
+                return [
+                    new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
+                    new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
+                ]
+            }
+        },
+        '.' // triggered whenever a '.' is being typed
+    )
+
     const provider1 = vscode.languages.registerCompletionItemProvider(
         sel,
         {
@@ -74,6 +105,7 @@ exports.activate = function (context) {
     )
 
     context.subscriptions.push(
+        provider2, // TODO rename
         provider1, // TODO rename
         providerHover,
         providerSymbol
